67915443cd8c503511406cfbb5a6b9d13be4f3ad
Dina Pavlyukovich <109734500+dinapavlyukovich@users.noreply.github.com>
2023-04-01 22:55:34 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+
+new Swiper('.swiper-container-1', {
+
+    pagination: {
+       el: '.swiper-pagination-1',
+       clickable: true,
+    },
+    slidesPerView: 1.2,
+
+ });
+
+ new Swiper('.swiper-container-2', {
+
+    pagination: {
+       el: '.swiper-pagination-2',
+       clickable: true,
+    },
+    slidesPerView: 1.2,
+
+ });
+
+ new Swiper('.swiper-container-3', {
+
+    pagination: {
+       el: '.swiper-pagination-3',
+       clickable: true,
+    },
+    slidesPerView: 1.2,
+
+ });
